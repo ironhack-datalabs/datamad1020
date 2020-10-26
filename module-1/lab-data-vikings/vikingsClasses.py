06c37bd4c25326859c4bd4b4a9542b24c8c995ed
Cristina Arenas Izquierdo <cristina.arenas.izq@gmail.com>
2020-10-26 17:13:30 +0100

    Ejercicio 1

--- module-1/lab-data-vikings/vikingsClasses.py
+++ module-1/lab-data-vikings/vikingsClasses.py
@@ -3,13 +3,41 @@
 
 
 class Soldier:
-    pass
+    def __init__(self, health, strength):
+        self.health = health
+        self.strength = strength
+        
+
+    def attack(self):
+        return self.strength
+
+    def receiveDamage(self, damage):
+        self.damage = damage
+        self.health -= self.damage
+
+        
 
 # Viking
 
 
-class Viking:
-    pass
+class Viking(Soldier):
+    def __init__(self, name, health, strength):
+        self.health = []
+        self.strength = strength
+
+    def attack(self):
+        return self.strength
+
+    def receiveDamage(self, damage):
+        self.damage = damage
+        if damage in self.health:
+            self.health.remove(damage)
+
+    def battleCry(self, damage):
+        self.damage = damage
+        if damage in self.health:
+            self.health.remove(damage)
+
 
 # Saxon
 
