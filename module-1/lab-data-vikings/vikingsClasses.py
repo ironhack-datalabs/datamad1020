586dcebc421c437e56cfcbada9bf5ed1493c4ae4
nachordo <ordovaspascual@gmail.com>
2020-10-26 17:14:32 +0100

    War part completed

--- module-1/lab-data-vikings/vikingsClasses.py
+++ module-1/lab-data-vikings/vikingsClasses.py
@@ -1,3 +1,4 @@
+import random
 
 # Soldier
 
@@ -52,11 +53,90 @@ class Saxon(Soldier):
 
 
 class War:
-    pass
+    def __init__(self):      
+        self.vikingArmy =  []
+        self.saxonArmy = []
+
+    def addViking(self,a_viking):
+        self.vikingArmy.append(a_viking)
+    
+    def addSaxon(self,a_saxon):
+        self.saxonArmy.append(a_saxon)
+        
+    def vikingAttack(self):
+        attacker = random.sample(self.vikingArmy, 1)[0]
+        defender = random.sample(self.saxonArmy, 1)[0]
+        action = defender.receiveDamage(attacker.attack())
+        if defender.health <= 0:
+            self.saxonArmy.remove(defender)
+        return action
+        
+    def saxonAttack(self):
+        attacker = random.sample(self.saxonArmy, 1)[0]
+        defender = random.sample(self.vikingArmy, 1)[0]
+        action = defender.receiveDamage(attacker.attack())
+        if defender.health <= 0:
+            self.vikingArmy.remove(defender)
+        return action
+            
+    def showStatus(self):
+        if self.saxonArmy == []:
+            return  "Vikings have won the war of the century!"
+        elif self.vikingArmy == []:
+            return "Saxons have fought for their lives and survive another day..."
+        else:
+            return "Vikings and Saxons are still in the thick of battle."
 
 
 
 
+
+
+guerra = War()
+
+
+viky1 = Viking("The Warrior of the Barrio",60,40)
+viky2 = Viking("Goteulfo el potras",60,40)
+viky3 = Viking("Sigmundo el imprudente",60,40)
+viky4 = Viking("Vikingo indistinto nº42",60,40)
+
+saja1 = Saxon(60,40)
+saja2 = Saxon(60,40)
+saja3 = Saxon(60,40)
+saja4 = Saxon(60,40)
+
+
+guerra.addViking(viky1)
+guerra.addViking(viky2)
+guerra.addViking(viky3)
+guerra.addViking(viky4)
+
+guerra.addSaxon(saja1)
+guerra.addSaxon(saja2)
+guerra.addSaxon(saja3)
+guerra.addSaxon(saja4)
+
+print(guerra.vikingAttack())
+
+print(guerra.saxonAttack())
+
+
+print(guerra.vikingAttack())
+
+print(guerra.saxonAttack())
+
+print(guerra.vikingAttack())
+
+print(guerra.saxonAttack())
+print(guerra.showStatus())
+
+
+
+
+
+
+
+"""
 
 #Purria para probar
 
@@ -70,3 +150,4 @@ print("\n\n")
 pringadete = Saxon(60,15)
 print(pringadete.receiveDamage(40))
 print(pringadete.receiveDamage(40))
+"""
