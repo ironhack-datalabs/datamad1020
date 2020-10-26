4d0dbf03bb95d4cb5900f58688934bfe8f823f6d
nachordo <ordovaspascual@gmail.com>
2020-10-26 16:11:02 +0100

    Viking part completed

--- module-1/lab-data-vikings/vikingsClasses.py
+++ module-1/lab-data-vikings/vikingsClasses.py
@@ -18,8 +18,23 @@ class Soldier:
 # Viking
 
 
-class Viking:
-    pass
+class Viking(Soldier):
+    def __init__(self, name, health, strength):
+        super().__init__(health, strength)        
+        self.name = name
+
+    def receiveDamage(self,damage):
+        self.health -= damage
+        if self.health > 0:
+            return self.name+" has received "+str(damage)+" points of damage"
+        else:
+            return self.name+" has died in act of combat"
+
+    def battleCry(self,damage):
+        self.health -= damage
+
+    def battleCry(self):
+        return "Odin Owns You All!" 
 
 # Saxon
 
@@ -38,8 +53,8 @@ class War:
 
 
 #Purria para probar
-warrior_of_the_barrio = Soldier(60,15)
-print(warrior_of_the_barrio.attack())
 
-print(warrior_of_the_barrio.receiveDamage(20))
-print(warrior_of_the_barrio.health)
+viky = Viking("The Warrior of the Barrio",60,15)
+viky.receiveDamage(30)
+viky.battleCry()
+viky.receiveDamage(40)
