0a932dc2e9d064b4002926e87006d11d0fa6688d
Cristina Arenas Izquierdo <cristina.arenas.izq@gmail.com>
2020-10-26 20:30:24 +0100

    Ejercicio 4 - War

--- module-1/lab-data-vikings/vikingsClasses.py
+++ module-1/lab-data-vikings/vikingsClasses.py
@@ -15,7 +15,8 @@ class Soldier:
         self.damage = damage
         self.health -= self.damage
 
-        
+
+  
 
 # Viking
 
@@ -36,6 +37,9 @@ class Viking(Soldier):
 
     def battleCry(self):
         return "Odin Owns You All!"
+
+
+
 
 
 # Saxon
@@ -62,4 +66,38 @@ class Saxon(Soldier):
 
 
 class War:
-    pass
+    def __init__(self):
+        self.vikingArmy = []
+        self.saxonArmy = []
+
+    def addViking(self, Viking):
+        self.vikingArmy.append(Viking)
+
+    def addSaxon(self, Saxon):
+        self.saxonArmy.append(Saxon)
+
+    def vikingAttack(self):
+        import random
+        saxon = random.choice(self.saxonArmy)
+        viking = random.choice(self.vikingArmy)
+        r = saxon.receiveDamage(viking.strength)
+        if saxon.health < 1:
+            self.saxonArmy.remove(saxon)
+        return r
+
+    def saxonAttack(self):
+        import random
+        saxon = random.choice(self.saxonArmy)
+        viking = random.choice(self.vikingArmy)
+        r = viking.receiveDamage(saxon.strength)
+        if viking.health < 1:
+            self.vikingArmy.remove(viking)
+        return r
+
+    def showStatus(self):
+        if self.saxonArmy == []:
+            return "Vikings have won the war of the century!"
+        if self.vikingArmy == []:
+            return "Saxons have fought for their lives and survive another day..."
+        if len(self.saxonArmy) > 0 and len(self.vikingArmy) > 0:
+            return "Vikings and Saxons are still in the thick of battle."
