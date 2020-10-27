a245a73372fe1f90fd23bad6009f1899ee61bade
BorjaRio <borjadelrio@hotmail.com>
2020-10-27 18:59:20 +0100

    done

--- module-1/lab-numpy/your-code/main.py
+++ module-1/lab-numpy/your-code/main.py
@@ -1,68 +1,83 @@
 #1. Import the NUMPY package under the name np.
 
-
+import numpy as np
 
 #2. Print the NUMPY version and the configuration.
 
-
+print(np.__version__)
+print(np.show_config())
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
 
-
+a= np.random.randint(1,10,(2,3,5))
 
 #4. Print a.
 
-
+print(a)
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
 
-
+b= np.ones((5,2,3))
 
 #6. Print b.
 
-
+print(b)
 
 #7. Do a and b have the same size? How do you prove that in Python code?
 
+def arrays_equal(a, b):
+    if a.shape != b.shape:
+        return False
+    else:
+        return True
 
-
+print(arrays_equal(a,b))
 
 #8. Are you able to add a and b? Why or why not?
 
-
+#we can´t add a and b , there is a ValueError: operands could not be broadcast together with shapes
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
 
+c=np.transpose(b,(1, 2, 0))
 
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
 
-
+d= a+c
+print(d)
+#it works because it has the same shape
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
 
-
+print(a)
+print(d)
 
 
 #12. Multiply a and c. Assign the result to e.
 
+e=a*c
+print(e)
 
 
 #13. Does e equal to a? Why or why not?
 
-
+#yes, they are equal
 
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
 
+d_max= np.max(d)
+d_min= np.min(d)
+d_mean= np.mean(d)
 
 
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
 
-
+f=np.empty([2,3,5])
 
 
 """
@@ -75,7 +90,17 @@ In the end, f should have only the following values: 0, 25, 50, 75, and 100.
 Note: you don't have to use Numpy in this question.
 """
 
-
+for index, value in np.ndenumerate(d):
+    if (value > d_min) & (value < d_mean):
+        print (index, 25)
+    elif (value == d_mean):
+        print(index, 50)
+    elif (value > d_mean) & (value < d_max):
+        print(index, 75)
+    elif (value == d_min):
+        print(index, 0)
+    else: 
+        print(100) 
 
 
 """
