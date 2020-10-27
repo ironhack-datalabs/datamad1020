855e994cfe4f87a5ef95d18680d3afd07bfe4a8f
arroyogonzalo41@gmail.com <arroyogonzalo41@gmail.com>
2020-10-27 08:56:41 +0100

    malditos vikingos

--- module-1/lab-data-vikings/vikingsClasses.py
+++ module-1/lab-data-vikings/vikingsClasses.py
@@ -3,22 +3,57 @@
 
 
 class Soldier:
-    pass
 
+    def __init__(self,health,strength):
+        self.health = health
+        self.strength = strength
+
+    def attack(self):
+        return self.strength
+    
+    def receiveDamage(self,damage):
+        self.health -= damage
+
+    
 # Viking
 
 
-class Viking:
-    pass
+class Viking(Soldier):
+
+    def __init__(self,name,health,strength):
+        super().__init__(health,strength)
+        self.name = name
+    
+    def receiveDamage(self,damage):
+
+        self.health -= damage
+
+        if self.health > 0:
+            return f"{self.name} has received {damage} points of damage"
+        else:
+            return f"{self.name} has dies in act of combat"
+    
+    def battleCry(self):
+        return "Odin Owns You All!"
 
 # Saxon
 
 
-class Saxon:
-    pass
+class Saxon(Soldier):
+
+    def receiveDamage(self,damage):
+
+        self.health -= damage
+
+        if self.health > 0:
+            return f"Saxon has received {damage} points of damage"
+        else:
+            return "Saxon has dies in act of combat" 
+
+
+        
 
 # War
 
-
 class War:
-    pass
+   pass
