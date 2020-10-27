5a0d71840da1abd1d6992773d481383d64d2c0ca
AdrianMadrid <adrian_6996@hotmail.com>
2020-10-27 01:42:34 +0100

    3 primeros

--- module-1/lab-data-vikings/vikingsClasses.py
+++ module-1/lab-data-vikings/vikingsClasses.py
@@ -3,22 +3,102 @@
 
 
 class Soldier:
-    pass
+	def __init__(self,health, strength):
+		self.health = health
+		self.strength = strength
+	
+	def attack(self):
+		return self.strength
+	
+	def receiveDamage(self,damage):
+		self.damage = damage
+		
+		
+		self.health -= self.damage
+
+		
+
+    
 
 # Viking
 
 
-class Viking:
-    pass
+class Viking(Soldier):
+	def __init__(self,name,health,strength):
+		super().__init__(health,strength)
+		self.name = name
+	def receiveDamage(self,damage):
+		self.damage = damage
+		self.health -= self.damage
+		if self.health == 0:
+			return f"{self.name} has died in act of combat"
+			
+		else:
+			return f"{self.name} has received {self.damage} points of damage"
+	def battleCry(self):
+		
+		return "Odin Owns You All!"
+
 
 # Saxon
+class Saxon(Soldier):
+	def __init__(self,health,strength):
+		super().__init__(health,strength)
+	def receiveDamage(self,damage):
+		self.damage = damage
+		self.health-= self.damage
+		
+		if self.health == 0:
+			return f"A Saxon has died in combat"
+		else: 
+			return f"A Saxon has received {self.damage} points of damage"
 
-
-class Saxon:
-    pass
 
 # War
 
 
 class War:
-    pass
+	def __init__(self):
+		self.vikingArmy = []
+		self.saxonArmy = []
+	def addViking(self,Viking):
+		self.Viking = Viking
+		self.vikingArmy.append(self.Viking)
+
+	def addSaxon(self,Saxon):
+		self.Saxon = Saxon
+		self.saxonArmy.append(self.Saxon)
+	def vikingAttack(self):
+		if (self.Saxon.health - self.Viking.damage) == self.Viking.strength:
+			saxonArmy.remove(self.Saxon)
+
+			
+			
+			 
+	def saxonAttack(self):
+		self.Saxon.health - self.Viking.strength
+			
+	def showStatus(self):
+		if len(saxonArmy) == 0:
+			return "Vikings have won the war of the century!"
+		if len(vikingArmy) == 0:
+			return "Saxons have fought for their lives and survive another day..."
+		if  len(vikingArmy) >= 1 and len(saxonArmy) >=1:
+			return "Vikings and Saxons are still in the thick of battle."
+
+
+
+
+
+
+		
+
+
+
+
+
+
+    	
+
+
+
