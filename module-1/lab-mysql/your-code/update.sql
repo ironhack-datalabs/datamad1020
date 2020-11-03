42bc3dc87e6f090f861740139d17afe2302790ac
nachordo <ordovaspascual@gmail.com>
2020-11-03 22:58:59 +0100

    Done everything but fails at invoices

---
+++ module-1/lab-mysql/your-code/update.sql
@@ -0,0 +1,9 @@
+UPDATE customer
+SET email = 'ppicasso@gmail.com'
+WHERE idc = 0;
+UPDATE customer
+SET email = 'lincoln@us.gov'
+WHERE idc = 1;
+UPDATE customer
+SET email = 'hello@napoleon.me'
+WHERE idc = 2;
