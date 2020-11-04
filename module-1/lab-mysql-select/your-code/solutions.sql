38e2b02e49177558c1619a6ed5ef4c8cdf94c6b1
nachordo <ordovaspascual@gmail.com>
2020-11-04 18:21:50 +0100

    Bonus completed

--- module-1/lab-mysql-select/your-code/solutions.sql
+++ module-1/lab-mysql-select/your-code/solutions.sql
@@ -34,7 +34,7 @@ FROM lab_sql_select.titles as t
         
 
 -- CHALLENGE 3
-SELECT a.au_id AS `AUTHOR ID`, a.au_lname AS `LAST NAME`, a.au_fname AS `FIRST NAME`, SUM(s.qty) as `TOTAL`  
+SELECT a.au_id AS `AUTHOR ID`, a.au_lname AS `LAST NAME`, a.au_fname AS `FIRST NAME`, SUM(t.ytd_sales) as `TOTAL`  
 FROM lab_sql_select.titles as t
 	 JOIN lab_sql_select.titleauthor AS ta
 		ON ta.title_id = t.title_id
@@ -45,19 +45,30 @@ FROM lab_sql_select.titles as t
 	 JOIN lab_sql_select.sales AS s
 		ON t.title_id = s.title_id        
 GROUP BY `AUTHOR ID`, p.pub_name
-ORDER BY SUM(s.qty) DESC
+ORDER BY SUM(t.ytd_sales) DESC
 LIMIT 3;
 
 -- CHALLENGE 4
-SELECT a.au_id AS `AUTHOR ID`, a.au_lname AS `LAST NAME`, a.au_fname AS `FIRST NAME`, SUM(s.qty) as `TOTAL`    
+SELECT a.au_id AS `AUTHOR ID`, a.au_lname AS `LAST NAME`, a.au_fname AS `FIRST NAME`, SUM(t.ytd_sales) as `TOTAL`    
 FROM lab_sql_select.titles as t
 	 JOIN lab_sql_select.titleauthor AS ta
 		ON ta.title_id = t.title_id
 	 JOIN lab_sql_select.authors AS a
 		ON ta.au_id = a.au_id
 	 JOIN lab_sql_select.publishers AS p
-		ON t.pub_id = p.pub_id
-	 JOIN lab_sql_select.sales AS s
-		ON t.title_id = s.title_id        
+		ON t.pub_id = p.pub_id      
 GROUP BY `AUTHOR ID`, p.pub_name
-ORDER BY SUM(s.qty) DESC;
+ORDER BY SUM(t.ytd_sales) DESC;
+
+-- BONUS
+SELECT a.au_id AS `AUTHOR ID`, a.au_lname AS `LAST NAME`, a.au_fname AS `FIRST NAME`, (SUM(t.ytd_sales*t.royalty+t.advance)) as `PORFIT`    
+FROM lab_sql_select.titles as t
+	 JOIN lab_sql_select.titleauthor AS ta
+		ON ta.title_id = t.title_id
+	 JOIN lab_sql_select.authors AS a
+		ON ta.au_id = a.au_id
+	 JOIN lab_sql_select.publishers AS p
+		ON t.pub_id = p.pub_id      
+GROUP BY `AUTHOR ID`, p.pub_name  -- Might come back later, the selected 
+ORDER BY `PORFIT` DESC
+LIMIT 3;
