8ac8060656d5f03b22ad8166751dc1a663450707
Angela Gomez <angelagomezfdz@gmail.com>
2020-11-07 17:19:41 +0100

    primera parte done

--- module-1/lab-api-scavenger-game/challenge-1.py
+++ module-1/lab-api-scavenger-game/challenge-1.py
@@ -1,1 +1,10 @@
-# enter your code below
+# importing libraries
+from dotenv import load_dotenv
+import os
+import re
+import pandas as pd
+
+
+# loading dotenv where my token is secure
+load_dotenv()
+
