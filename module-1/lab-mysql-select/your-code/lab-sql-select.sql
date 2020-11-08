43ffa723bde5550a18f1674aeda7f4f7678b827e
Cande García <candelagarciafdez@gmail.com>
2020-11-08 18:55:28 +0100

    [correccion-lab-mysql-select] Candela García

--- module-1/lab-mysql-select/your-code/lab-sql-select.sql
+++ module-1/lab-mysql-select/your-code/lab-sql-select.sql
@@ -1,64 +1,60 @@
 
 
--- CHALLENGE 1
+-- CHALLENGE  1
+SELECT  p.au_id AS "AUTHOR ID",
+        p.au_fname AS "FIRST NAME",
+        p.au_lname AS "LAST NAME",
+		titles.title AS "TITLE",
+		publishers.pub_name AS "PUBLISHER"
 
+FROM publications.authors AS p
+JOIN titleauthor
+ON p.au_id = titleauthor.au_id
+JOIN titles
+ON titleauthor.title_id = titles.title_id
+JOIN publishers
+ON titles.pub_id = publishers.pub_id;
 
-CREATE TABLE mitabla(
-SELECT au_id, au_lname, au_fname  FROM authors AS a
-JOIN titleauthor AS t USING(au_id));
+--CHALLENGE  2
+SELECT  p.au_id AS "AUTHOR ID",
+        p.au_fname AS "FIRST NAME",
+        p.au_lname AS "LAST NAME",
+        publishers.pub_name AS "PUBLISHER" ,
+COUNT(DISTINCT titles.title) AS "TITLE COUNT"
+FROM publications.authors AS p
+JOIN titleauthor
+ON p.au_id = titleauthor.au_id
+JOIN titles
+ON titleauthor.title_id = titles.title_id
+JOIN publishers
+ON titles.pub_id = publishers.pub_id
+GROUP BY "AUTHOR ID", PUBLISHER;
 
+--CHALLENGE  3
+SELECT  p.au_id AS "AUTHOR ID",
+	    p.au_fname AS "FIRST NAME",
+     	p.au_lname AS "LAST NAME",
+        SUM(DISTINCT s.qty) AS `TOTAL`
 
-CREATE TABLE challenge(
-SELECT * FROM bookstore.titles
-JOIN mitabla USING (title_id));
+FROM publications.authors AS p
+JOIN titleauthor AS t
+ON p.au_id = t.au_id
+JOIN sales AS s
+ON t.title_id = s.title_id
+GROUP BY "AUTHOR ID"
+ORDER BY `TOTAL` DESC
+LIMIT 3;
 
-CREATE TABLE challenge1(
-SELECT * FROM bookstore.publishers
-JOIN challenge USING (pub_id));
+-- CHALLENGE  4
+SELECT  p.au_id AS "AUTHOR ID",
+        p.au_fname AS "FIRST NAME",
+        p.au_lname AS "LAST NAME",
+        SUM(DISTINCT s.qty) AS `TOTAL`
 
-SELECT * FROM bookstore.challenge1;
-ALTER TABLE challenge1
- DROP pub_id,
- DROP city,
- DROP state,
- DROP country,
- DROP title_id,
- DROP type,
- DROP price,
- DROP advance,
- DROP royalty,
- DROP ytd_sales,
- DROP  notes,
- DROP pubdate,
- DROP au_ord,
- DROP royaltyper;
-
--- APARECEN ALGUNOS LIBROS DUPLICADOS (NO SÉ EL MOTIVO) ASÍ QUE HE UTILIZADO :
-
-SELECT DISTINCT* FROM bookstore.challenge1;
-
-
-
---CHALLENGE 2
-SELECT AUTHOR_ID, LAST_NAME, FIRST_NAME, PUBLISHER ,
-COUNT(DISTINCT title) AS `TITLE COUNT`
-FROM challenge1
-GROUP BY AUTHOR_ID, PUBLISHER ;
-
-
-
---CHALLENGE3
-
-SELECT au_id AS AUTHOR_ID, au_fname AS FIRST_NAME, au_lname AS LAST_NAME, SUM(ytd_sales) AS TOTAL
-FROM bookstore.challenge
-GROUP BY (au_id)
-ORDER BY TOTAL DESC
-LIMIT 3 ;
-
-
--- CHALLENGE4
-
-SELECT au_id AS AUTHOR_ID, au_fname AS FIRST_NAME, au_lname AS LAST_NAME, SUM(IFNULL(ytd_sales,0)) AS TOTAL
-FROM bookstore.challenge
-GROUP BY (au_id)
-ORDER BY TOTAL DESC;
+FROM publications.authors AS p
+JOIN titleauthor AS t
+ON p.au_id = t.au_id
+JOIN sales AS s
+ON t.title_id = s.title_id
+GROUP BY "AUTHOR ID"
+ORDER BY `TOTAL` DESC;
