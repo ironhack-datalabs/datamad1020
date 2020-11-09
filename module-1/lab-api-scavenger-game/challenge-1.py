acf4475a4bfa88bd2455102e2a7f43f55e19cd23
BorjaRio <borjadelrio@hotmail.com>
2020-11-09 20:07:18 +0100

    done

--- module-1/lab-api-scavenger-game/challenge-1.py
+++ module-1/lab-api-scavenger-game/challenge-1.py
@@ -1,1 +1,9 @@
-# enter your code below
+import requests
+import os
+data = requests.get("https://github.com/ironhack-datalabs/datamad1020")
+
+
+from  dotenv import load_dotenv
+load_dotenv ()
+token = os.environ.get ( "api-token" )
+
